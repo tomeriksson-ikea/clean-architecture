26d477c69a949dbafd5346b6c18b886ea47ec413
Tom Eriksson <tom.eriksson@ingka.ikea.com>
2025-02-22 22:44:21 +0100

    fix: remove undefined from type

--- src/repositories/Class.repository.interface.ts
+++ src/repositories/Class.repository.interface.ts
@@ -2,6 +2,6 @@ import { IClass } from "../entities/Class/Class.interface";
 
 export interface IClassRepository {
   addClass(_class: IClass): Promise<IClass>;
-  getClass(classCode: string): Promise<IClass | undefined>;
+  getClass(classCode: string): Promise<IClass>;
   saveClass(_class: IClass): Promise<IClass>;
 }
